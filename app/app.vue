e02ee8025868a4e4263ba76391e22513e70c8d47
Geoff 'Kit Kabbit' Lambert <kitkabbit@pm.me>
2026-07-19 20:38:10 -0600

    added navbar (non-functional)

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      <Icon name="streamline-plump:pet-paw" size="48" />
-      Foxtrax
-      <Icon name="streamline-plump:pet-paw-solid" size="48" />
-    </h1>
-    <h2 class="text-xl font-semibold">
-      A truly atrocious demo page to ensure all my modules are correctly imported!
-    </h2>
-    <button class="btn btn-primary">
-      View Map <Icon name="streamline-plump:map-fold" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        <Icon name="streamline-plump:pet-paw" size="24" />
+        Foxtrax
+        <Icon name="streamline-plump:pet-paw-solid" size="24" />
+      </h1>
+      <h2 class="text-xl font-semibold">
+        A truly atrocious demo page to ensure all my modules are correctly imported!
+      </h2>
+      <button class="btn btn-primary">
+        View Map <Icon name="streamline-plump:map-fold" />
+      </button>
+    </div>
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+function navigateHome() {
+  const router = useRouter();
+
+  if (router.currentRoute.value.path !== "/") {
+    router.push("/");
+  }
+}
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <div class="navbar bg-neutral text-neutral-content">
+      <div class="navbar-start">
+        <button class="btn btn-ghost text-xl" @click="navigateHome">
+          <Icon name="streamline-plump:pet-paw" class="-mt-2" size="24" />
+          Foxtrax
+          <Icon name="streamline-plump:pet-paw-solid" class="mt-2" size="24" />
+        </button>
+      </div>
+      <div class="navbar-end">
+        <button class="btn btn-primary">
+          Login <Icon name="streamline-plump:login-1" size="18" />
+        </button>
+      </div>
+    </div>
+    <main class="flex-1">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
